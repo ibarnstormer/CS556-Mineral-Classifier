44a26de4b1b4d410254ae00dc365a007f9ad640c
ibarnstormer <48729319+ibarnstormer@users.noreply.github.com>
2025-02-09 16:13:18 -0500

    Updated slurm script, added gitignore

--- slurm scripts/cs556-main-run.sh
+++ slurm scripts/cs556-main-run.sh
@@ -2,11 +2,11 @@
 #SBATCH -N 1
 #SBATCH -n 25
 #SBATCH --mem=12g
-#SBATCH -J "SIIM-ACR Pneumothorax FasterViT"
+#SBATCH -J "CS 556 Mineral Classifier model"
 #SBATCH -p long
 #SBATCH -t 2-23:00:00
-#SBATCH --gres=gpu:2
-#SBATCH -C A100|V100|A30
+#SBATCH --gres=gpu:1
+#SBATCH -C H100|L40S|A100|V100
 
 module load python/3.10.12/f5uihwq
 module load cuda11.2/blas/11.2.2
